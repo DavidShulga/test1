e6039d17fbbb207407524b82d83fb6068ae105f1
David <74916391+DavidShulga@users.noreply.github.com>
2021-09-22 20:51:33 +0300

    Changes

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Index from "pages/index";
+import Page7 from "pages/page7";
+import Page8 from "pages/page8";
+import Page404 from "pages/page404";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+    }
+`;
+
+export default () => (
+    <Router>
+        <GlobalStyles />
+        <Switch>
+        			<Route exact path='/' component={Index}/>
+			<Route exact path='/index' component={Index}/>
+			<Route exact path='/7' component={Page7}/>
+			<Route exact path='/8' component={Page8}/>
+			<Route component={Page404}/>
+        </Switch>
+    </Router>
+);
---
+++ src/qapi.js
@@ -0,0 +1,41 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"614b69ed71cf90001f32064f",
+				"614b69ed71cf90001f320651",
+				"614b6d1f8bdb79001eb18ea1",
+				"614b6d208bdb79001eb18ea3"
+			]
+		},
+		"614b69ed71cf90001f32064f": {
+			"id": "614b69ed71cf90001f32064f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"614b69ed71cf90001f320651": {
+			"id": "614b69ed71cf90001f320651",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"614b6d1f8bdb79001eb18ea1": {
+			"id": "614b6d1f8bdb79001eb18ea1",
+			"pageUrl": "7",
+			"name": "7"
+		},
+		"614b6d208bdb79001eb18ea3": {
+			"id": "614b6d208bdb79001eb18ea3",
+			"pageUrl": "8",
+			"name": "8"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
